42cf81b00a4f0aef787ff134f74e99db4489ad87
Jinwoo Chang <83801879+jinw0909@users.noreply.github.com>
2024-09-12 19:17:36 +0900

    240912 commit

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -9,8 +9,12 @@ class Cors
 {
     public function handle(Request $request, Closure $next)
     {
-        $response = $next($request);
-
+        // Handle preflight request
+        if ($request->getMethod() === "OPTIONS") {
+            $response = response('', 200);
+        } else {
+            $response = $next($request);
+        }
         $response->headers->set('Access-Control-Allow-Origin', '*');
         $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Origin, Authorization');
